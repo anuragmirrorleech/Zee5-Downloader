b1d40cb7d4ec4600049faf0fdc3a6e114bc215c9
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2023-01-25 00:53:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,5 +29,5 @@ class Config(object):
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
     
     # Sql Database url
-    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://rename:rename@cluster0.f0vmafy.mongodb.net/?retryWrites=true&w=majority")
+    DB_URI = os.environ.get("DATABASE_URL", "postgres://xofbrbld:FVl475QBPaJI_Zm1yFzupV6FO1h3fLfp@lucky.db.elephantsql.com/xofbrbld")
     
